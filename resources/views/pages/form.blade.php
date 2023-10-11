c6c152f4df1ff8cc7ece2e79e481916d3fd48d74
mcristobaln <mcristobal@outlook.com>
2023-10-11 09:54:17 -0500

    feat: Add btn the wlecome

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -4,7 +4,7 @@
         <div class="container mb-5 p-5 paper">
             <div class="row">
                 <div class="col-md-3 d-flex">
-                    <i class="fa-solid fa-person-running icon-main"></i><div><p class="logo logo-short">MARATON INTENACIONAL</p><p class="logo logo-short"></p><p class="logo logo-secondary">MESETA DEL</p><p class="logo logo-main">BOMBOM</p><p class="logo logo-date">Noviembre, 5 2023</p></div>
+                    <a class="navbar-brand d-flex" href="{{route('welcome')}}"><i class="fa-solid fa-person-running icon-main"></i><div><p class="logo logo-short">MARATON INTENACIONAL</p><p class="logo logo-short"></p><p class="logo logo-secondary">MESETA DEL</p><p class="logo logo-main">BOMBON</p><p class="logo logo-date">Noviembre, 5 2023</p></div></a>
                 </div>
                 <div class="col-md-9 mt-3">
                     <h2 class="text-center title-form">FICHA DE INSCRIPCION PARA LA MARATON</h2>
